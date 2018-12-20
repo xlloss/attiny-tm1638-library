c27009f35267637a2cc16286565618648a5c9649
xlloss <xlloss@gmail.com>
2018-12-20 09:19:07 +0800

    add user space test code

--- test_code/tm1638_test.c
+++ test_code/tm1638_test.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
 #include "../tm1638.h"
 
 #define DEVFILE "/dev/tm1638_dev"
@@ -10,17 +11,30 @@ int main(void)
 {
     int fd;
     int ret;
+    struct display_digit *dis_show;
+    int i;
 
+    dis_show = (struct display_digit *) malloc(sizeof(*dis_show));
     fd = open(DEVFILE, O_RDWR);
-    if (fd == -1) {
-            perror("open");
+    if (fd == -1)
+        perror("open");
+
+
+    dis_show->position = 0;
+    dis_show->dot = 1;
+
+    for (i = 0; i < 10; i++) {
+        dis_show->digit = i;
+        ret = ioctl(fd, IOCTL_DISPLAY_SEG, dis_show);
+        sleep(1);
     }
 
-    ret = ioctl(fd, IOCTL_DISPLAY_SEG, NULL);
+    getchar();
+    if (close(fd))
+        perror("close");
 
-    if (close(fd) != 0) {
-            perror("close");
-    }
+    if (dis_show)
+        free(dis_show);
 
     return 0;
 }
