24ea83a05835dbbc046ad4f19c732cab6b39721b
Slash.Huang <slash.huang@dfi.com>
2019-08-22 17:29:08 +0800

    porting to kernel 4.20.7

--- tm1638.c
+++ tm1638.c
@@ -26,14 +26,41 @@
 #include <linux/miscdevice.h>
 #include <linux/fcntl.h>
 #include <linux/mm.h>
-#include <linux/gpio.h>
-#include <asm/uaccess.h>
-#include <mach/gpio.h>
-#include <plat/gpio-cfg.h>
 #include <linux/errno.h>
 #include <linux/delay.h>
+#include <linux//uaccess.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_device.h>
+#include <linux/of_gpio.h>
 #include "tm1638.h"
 
+#define PIN_NUM 3
+#define PIN_DIO_DTS_NAME "gpio_dio"
+#define PIN_SCK_DTS_NAME "gpio_sck"
+#define PIN_STB_DTS_NAME "gpio_stb"
+
+#define GPIO_OUT_HI 1
+#define GPIO_OUT_LO 0
+
+#define TM1638_SEG_0 0x3F
+#define TM1638_SEG_1 0x06
+#define TM1638_SEG_2 0x5B
+#define TM1638_SEG_3 0x4F
+#define TM1638_SEG_4 0x66
+#define TM1638_SEG_5 0x6D
+#define TM1638_SEG_6 0x7D
+#define TM1638_SEG_7 0x07
+#define TM1638_SEG_8 0x7F
+#define TM1638_SEG_9 0x6F
+#define TM1638_SEG_DOT 0x80
+
+#define TM1638_LED_NUM 8
+#define TM1638_LED_CLR 0
+
+#define TM1638_SEG_NUM 10
+
+#define TM1638_MISCDEV_NAME "tm1638_dev"
 
 struct tm1638_display
 {
@@ -43,9 +70,9 @@ struct tm1638_display
 
 struct tm1638_data
 {
-	int gpio_dio;
-	int gpio_sck;
-    int gpio_stb;
+    struct gpio_desc *gpio_dio;
+    struct gpio_desc *gpio_sck;
+    struct gpio_desc *gpio_stb;
     struct device *dev;
 };
 
@@ -53,68 +80,75 @@ struct tm1638_data *data;
 
 static uint8_t _config = TM1638_SET_DISPLAY_ON | TM1638_MAX_BRIGHTNESS;
 
-const uint8_t _digit2segments[10] = {
-	0x3F, // 0
-	0x06, // 1
-	0x5B, // 2
-	0x4F, // 3
-	0x66, // 4
-	0x6D, // 5
-	0x7D, // 6
-	0x07, // 7
-	0x7F, // 8
-	0x6F  // 9
+const uint8_t _digit2segments[TM1638_SEG_NUM] = {
+	TM1638_SEG_0,
+	TM1638_SEG_1,
+	TM1638_SEG_2,
+	TM1638_SEG_3,
+	TM1638_SEG_4,
+	TM1638_SEG_5,
+	TM1638_SEG_6,
+	TM1638_SEG_7,
+	TM1638_SEG_8,
+	TM1638_SEG_9 
 };
-
 
 void tm1638_dio_output(void)
 {
-    s3c_gpio_cfgpin(data->gpio_dio, S3C_GPIO_OUTPUT);
+	int ret;
+
+	ret = gpiod_direction_output(data->gpio_dio, GPIO_OUT_HI);
+	if (ret)
+		pr_err("tm1638_dio_output change fail\r\n");
 }
 
 void tm1638_dio_input(void)
 {
-    s3c_gpio_cfgpin(data->gpio_dio, S3C_GPIO_INPUT);
+	int ret;
+
+	ret = gpiod_direction_input(data->gpio_dio);
+	if (ret)
+		pr_err("tm1638_dio_input change fail\r\n");
 }
 
 void tm1638_dio_high(void)
 {
-    gpio_set_value(data->gpio_dio, 1);
+    gpiod_set_value(data->gpio_dio, GPIO_OUT_HI);
     usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
 }
 
 void tm1638_dio_low(void)
 {
-    gpio_set_value(data->gpio_dio, 0);
+    gpiod_set_value(data->gpio_dio, GPIO_OUT_LO);
     usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
 }
 
 unsigned char tm1638_dio_read(void)
 {
-    return gpio_get_value(data->gpio_dio);
+    return gpiod_get_value(data->gpio_dio);
 }
 
 void tm1638_clk_high(void)
 {
-    gpio_set_value(data->gpio_sck, 1);
+    gpiod_set_value(data->gpio_dio, GPIO_OUT_HI);
     usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
 }
 
 void tm1638_clk_low(void)
 {
-    gpio_set_value(data->gpio_sck, 0);
+    gpiod_set_value(data->gpio_sck, GPIO_OUT_LO);
     usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
 }
 
 void tm1638_stb_high(void)
 {
-    gpio_set_value(data->gpio_stb, 1);
+    gpiod_set_value(data->gpio_sck, GPIO_OUT_HI);
     usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
 }
 
 void tm1638_stb_low(void)
 {
-    gpio_set_value(data->gpio_stb, 0);
+    gpiod_set_value(data->gpio_stb, GPIO_OUT_LO);
     usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
 }
 
@@ -122,18 +156,25 @@ uint8_t tm1638_read_byte(void)
 {
 	uint8_t i, result = 0;
 
+    tm1638_dio_high();
 	tm1638_dio_input();
-	tm1638_dio_high();
+    usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
 
 	for (i = 0; i < 8; i++) {
 		tm1638_clk_low();
-		if (tm1638_dio_read()) {
+        usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
+		if (tm1638_dio_read())
 			result |= 0x80;
-		}
-		result >>= 1;
+
+        if (i != 0)
+            result = result >> 1;
+
 		tm1638_clk_high();
+        usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
 	}
 
+
+    usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
 	tm1638_dio_output();
 	tm1638_dio_low();
 
@@ -229,13 +270,13 @@ void tm1638_display_digit(const uint8_t position, const uint8_t digit,
 {
     uint8_t segment;
 
-    if (digit < 10)
+    if (digit < TM1638_SEG_NUM)
         segment = _digit2segments[digit];
     else
         segment = 0;
 
     if (dot)
-        segment = segment | 0x80;
+        segment = segment | TM1638_SEG_DOT;
 
     tm1638_display_segments(position, segment);
 
@@ -250,19 +291,26 @@ void tm1638_clear_leds(void)
 {
 	uint8_t i;
 
-	for (i = 0; i < 8; ++i) {
-		tm1638_set_led(i, 0x00);
-	}
+	for (i = 0; i < TM1638_LED_NUM; ++i)
+		tm1638_set_led(i, TM1638_LED_CLR);
+
 }
 
 uint8_t tm1638_scan_keys(void)
 {
-	uint8_t i, keys = 0;
+	uint8_t i;
+	uint8_t keys = 0;
+//    uint8_t mykey[8] = {0};
 
-  	tm1638_stb_low();
-  	tm1638_write_byte(TM1638_CMD_SET_DATA|TM1638_SET_DATA_READ);
-	for (i = 0; i < 4; i++) {
-		keys |= tm1638_read_byte() << i;
+    tm1638_stb_low();
+    usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
+    tm1638_write_byte(TM1638_CMD_SET_DATA | TM1638_SET_DATA_READ);
+    usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
+    for (i = 0; i < 4; i++) {
+        usleep_range(TM1638_USLEEP_MIN, TM1638_USLEEP_MAX);
+//		keys |= tm1638_read_byte() << i;
+        pr_err("key %d : read 0x%x\r\n", i, tm1638_read_byte());
+//        mykey[keys] = 1;
 	}
 	tm1638_stb_high();
 
@@ -277,16 +325,15 @@ void tm1638_hw_init(const uint8_t enable, const uint8_t brightness)
 
 static int tm1638_open(struct inode *inode, struct file *file)
 {
-    pr_err("%s\r\n", __func__);
+    pr_info("%s\r\n", __func__);
 	return 0;
 }
 
 static int tm1638_release(struct inode *inode, struct file *file)
 {
-    pr_err("%s\r\n", __func__);
+    pr_info("%s\r\n", __func__);
 	return 0;
 }
-
 
 static long tm1638_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
@@ -308,12 +355,12 @@ static long tm1638_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
             break;
 
         case IOCTL_CLEAR_SEG:
-            pr_err("%s:IOCTL_CLEAR_SEG\r\n", __func__);
+            pr_info("%s:IOCTL_CLEAR_SEG\r\n", __func__);
             tm1638_clear_segments();
             break;
 
         case IOCTL_SET_LED:
-            pr_err("%s:IOCTL_SET_LED\r\n", __func__);
+            pr_info("%s:IOCTL_SET_LED\r\n", __func__);
             if (copy_from_user(&__tm1638_led, argp, sizeof(__tm1638_led)))
                 return -EFAULT;
 
@@ -322,19 +369,19 @@ static long tm1638_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
         case IOCTL_CLEAR_LED:
             tm1638_clear_leds();
-            pr_err("%s:IOCTL_CLEAR_LED\r\n", __func__);
+            pr_info("%s:IOCTL_CLEAR_LED\r\n", __func__);
             break;
 
         case IOCTL_SCAN_KEY:
-            pr_err("%s:IOCTL_SCAN_KEY\r\n", __func__);
+            pr_info("%s:IOCTL_SCAN_KEY\r\n", __func__);
             break;
 
         case IOCTL_SET_CONFIG:
-            pr_err("%s:IOCTL_SET_CONFIG\r\n", __func__);
+            pr_info("%s:IOCTL_SET_CONFIG\r\n", __func__);
             break;
 
         default:
-            pr_err("%s:nothing\r\n", __func__);
+            pr_info("%s:nothing\r\n", __func__);
             break;
     }
 
@@ -350,7 +397,7 @@ static const struct file_operations tm1638_fops = {
 
 static struct miscdevice tm1638_device = {
 	.minor = MISC_DYNAMIC_MINOR,
-	.name = "tm1638_dev",
+	.name = TM1638_MISCDEV_NAME,
 	.fops = &tm1638_fops,
 };
 
@@ -360,16 +407,16 @@ static struct miscdevice tm1638_device = {
  * data->gpio_stb -> EXYNOS4_GPB(6) --> XspiMISO1/i2cSDA5 --> EVB GPIO7
  * 
  */
- 
 static int tm1638_probe(struct platform_device *pdev)
 {
     int err;
-    int ret;
     int i;
+	char *pin_name[PIN_NUM] = {PIN_DIO_DTS_NAME,
+            PIN_SCK_DTS_NAME, PIN_STB_DTS_NAME};
 
-    pr_err("%s\r\n", __func__);
+    pr_info("%s\r\n", __func__);
 
-    data = kzalloc(sizeof(*data), GFP_KERNEL);
+    //data = kzalloc(sizeof(*data), GFP_KERNEL);
 
 	err = misc_register(&tm1638_device);
 	if (err) {
@@ -377,34 +424,34 @@ static int tm1638_probe(struct platform_device *pdev)
 		goto misc_register_failed;
 	}
 
-    platform_set_drvdata(pdev, data);
-    data->dev = &pdev->dev;
+    //platform_set_drvdata(pdev, data);
+    //data->dev = &pdev->dev;
 
-    data->gpio_dio = EXYNOS4_GPB(4);
-	ret = gpio_request(data->gpio_dio, "tm1638 dio");
-	if (ret) {
-		dev_err(&pdev->dev, "gpio dio request failed\n");
-		goto err_gpio;
+    /* Try requesting the GPIOs */
+	for (i = 0; i < PIN_NUM; i++) {
+		switch (i) {
+			case 0:
+				data->gpio_dio = devm_gpiod_get_optional(&pdev->dev,
+					pin_name[i], GPIOD_OUT_HIGH);
+				if (IS_ERR(data->gpio_dio))
+					goto gpio_init_error;
+				break;
+
+			case 1:
+				data->gpio_sck = devm_gpiod_get_optional(&pdev->dev,
+					pin_name[i], GPIOD_OUT_HIGH);
+				if (IS_ERR(data->gpio_sck))
+					goto gpio_init_error;
+				break;
+
+			case 2:
+				data->gpio_stb = devm_gpiod_get_optional(&pdev->dev,
+					pin_name[i], GPIOD_OUT_HIGH);
+				if (IS_ERR(data->gpio_stb))
+					goto gpio_init_error;
+				break;
+		}
 	}
-    gpio_direction_output(data->gpio_dio, 1);
-
-    data->gpio_sck = EXYNOS4_GPB(5);
-	/* Try requesting the GPIOs */
-	ret = gpio_request(data->gpio_sck, "tm1638 sck");
-	if (ret) {
-		dev_err(&pdev->dev, "gpio sck request failed\n");
-		goto err_gpio;
-	}
-	gpio_direction_output(data->gpio_sck, 1);
-
-
-    data->gpio_stb = EXYNOS4_GPB(6);
-	ret = gpio_request(data->gpio_stb, "tm1638 stb");
-	if (ret) {
-		dev_err(&pdev->dev, "gpio stb request failed\n");
-		goto err_gpio;
-	}
-    gpio_direction_output(data->gpio_stb, 1);
 
     tm1638_hw_init(TM1638_SET_DISPLAY_ON, TM1638_MAX_BRIGHTNESS);
 
@@ -412,13 +459,16 @@ static int tm1638_probe(struct platform_device *pdev)
         tm1638_display_digit(i * 2, 0, 1);
         tm1638_set_led(i, 1);
     }
+    /* pr_err("get key %d\r\n", tm1638_scan_keys()); */
 
     return 0;
 
-err_gpio:
-misc_register_failed:
+gpio_init_error:
+	dev_err(&pdev->dev, "%s gpio init failed index %d\r\n", __func__, i);
 
-    kfree(data);
+misc_register_failed:
+    dev_err(&pdev->dev, "%s misc_register_failed\r\n", __func__);
+//    kfree(data);
     return 0;
 }
 
@@ -427,17 +477,14 @@ static int tm1638_remove(struct platform_device *pdev)
     pr_err("%s\r\n", __func__);
 
     tm1638_clear_segments();
-    gpio_free(data->gpio_sck);
-    gpio_free(data->gpio_dio);
-    gpio_free(data->gpio_stb);
     misc_deregister(&tm1638_device);
-    kfree(data);
+//    kfree(data);
     return 0;
 }
 
 #ifdef CONFIG_OF
 static const struct of_device_id tm1638_match[] = {
-	{ .compatible = "test,tm1638" },
+	{ .compatible = "test, tm1638" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, tm1638_match);
@@ -459,4 +506,4 @@ static struct platform_driver tm1638_driver = {
 module_platform_driver(tm1638_driver);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Xlloss <xlloss@gmail.com>");
+MODULE_AUTHOR("Slash <slash.linux.c@gmail.com>");
--- tm1638.h
+++ tm1638.h
@@ -17,17 +17,10 @@
  * - TM1638 datasheet: https://github.com/lpodkalicki/attiny-tm1638-library/blob/master/docs/tm1638.pdf
  */
 
-#ifndef	_TM1638_H_
-#define	_TM1638_H_
+#ifndef _TM1638_H_
+#define _TM1638_H_
 
 #include <linux/ioctl.h>
-
-//#define IOCTL_DISPLAY_SEG 0xFF112200
-//#define IOCTL_CLEAR_SEG 0xFF112211
-//#define IOCTL_SET_LED 0xFF112222
-//#define IOCTL_CLEAR_LED 0xFF112233
-//#define IOCTL_SCAN_KEY 0xFF112244
-//#define IOCTL_SET_CONFIG 0xFF112255
 
 struct display_digit
 {
@@ -53,10 +46,7 @@ struct display_led
 
 
 #ifdef __KERNEL__
-// Main Settings
-#define	TM1638_DIO_PIN			PB0
-#define	TM1638_CLK_PIN			PB1
-#define	TM1638_STB_PIN			PB2
+/* Main Settings */
 #define	TM1638_DELAY_US			2
 #define	TM1638_MSLEEP			1
 #define	TM1638_USLEEP_MAX			20
@@ -71,24 +61,22 @@ struct display_led
 #define	TM1638_6_BRIGHTNESS 0x06
 #define	TM1638_MAX_BRIGHTNESS 0x07
 
+/* TM1638 commands */
+#define	TM1638_CMD_SET_DATA 0x40
+#define	TM1638_CMD_SET_ADDR 0xC0
+#define	TM1638_CMD_SET_DSIPLAY 0x80
 
-// TM1638 commands
-#define	TM1638_CMD_SET_DATA		0x40
-#define	TM1638_CMD_SET_ADDR		0xC0
-#define	TM1638_CMD_SET_DSIPLAY		0x80
+/* TM1638 data settings (use bitwise OR to contruct complete command) */
+#define	TM1638_SET_DATA_WRITE 0x00 // write data to the display register
+#define	TM1638_SET_DATA_READ 0x02 // read the key scan data
+#define	TM1638_SET_DATA_A_ADDR 0x00 // automatic address increment
+#define	TM1638_SET_DATA_F_ADDR 0x04 // fixed address
+#define	TM1638_SET_DATA_M_NORM 0x00 // normal mode
+#define	TM1638_SET_DATA_M_TEST 0x10 // test mode
 
-// TM1638 data settings (use bitwise OR to contruct complete command)
-#define	TM1638_SET_DATA_WRITE		0x00 // write data to the display register
-#define	TM1638_SET_DATA_READ		0x02 // read the key scan data
-#define	TM1638_SET_DATA_A_ADDR		0x00 // automatic address increment
-#define	TM1638_SET_DATA_F_ADDR		0x04 // fixed address
-#define	TM1638_SET_DATA_M_NORM		0x00 // normal mode
-#define	TM1638_SET_DATA_M_TEST		0x10 // test mode
-
-// TM1638 display control command set (use bitwise OR to consruct complete command)
-#define	TM1638_SET_DISPLAY_OFF		0x00 // off
-#define	TM1638_SET_DISPLAY_ON		0x08 // on
-
+/* TM1638 display control command set (use bitwise OR to consruct complete command) */
+#define	TM1638_SET_DISPLAY_OFF 0x00 // off
+#define	TM1638_SET_DISPLAY_ON 0x08 // on
 
 /**
  * Initialize TM1638 LED controller.
@@ -157,5 +145,5 @@ void TM1638_clear_leds(void);
  */
 uint8_t TM1638_scan_keys(void);
 
-#endif	/* _TM1638_H_ */
+#endif /* _TM1638_H_ */
 #endif
